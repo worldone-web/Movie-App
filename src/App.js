ed9db6ed0077d6a340a4f3fb278071447e4d09aa
Jae-Young-Kim <tjfansghk@naver.com>
2024-08-16 15:35:31 +0900

    theHeader component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Component } from "./core/heropy";
+import TheHeader from "./components/TheHeader";
 
 export default class App extends Component{
     render(){
+        const theHeader= new TheHeader().el
         const routerView =document.createElement('router-view') // 페이지 요소 객체 생성
-        this.el.append(routerView)
+        this.el.append(
+            theHeader,
+            routerView
+        )
     }
 }
