925148293a3f5d3295b121323ce8df739449ffc5
Jae-Young-Kim <tjfansghk@naver.com>
2024-08-16 16:16:25 +0900

    not Found component

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ import {createRouter} from '../core/heropy'
 import Home from './Home'
 import Movie from './Movie'
 import About from './About.js'
+import NotFound from './NotFound'
 
 export default createRouter([
     {path:'#/', component:Home},
     {path:'#/movie', component:Movie},
     {path:'#/about', component:About},
+    {path:'.*', component:NotFound},
 ])
+
